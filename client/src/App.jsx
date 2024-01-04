68a056e4e1c4b78355fe5847795bd639346f0a16
mdshahco <mdshahcoding.gmail.com>
2024-01-04 23:22:02 +0600

    create page and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,51 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Footer, Navbar } from "./components";
+import { UserProfile } from "./assets";
+import { About, Auth, Companies, CompanyProfile, FindJobs, JobDetail, UploadJob } from "./pages";
 
+function Layout() {
+  const user = true;
+  const location = useLocation();
+
+  return user ? (
+    <Outlet />
+  ) : (
+    <Navigate to="user-auth" state={{ from: location }} replace />
+  );
+}
 function App() {
-
+  const user ={};
   return (
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <main>
+      <Navbar />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route
+            path="/"
+            element={<Navigate to="/find-jobs" replace={true} />}
+          />
+          <Route path="/find-jobs" element={<FindJobs />} />
+          <Route path="/companies" element={<Companies />} />
+          <Route
+            path={
+              user?.user?.accountType == "seeker"
+                ? "/user-profile"
+                : "user-profile/:id"
+            }
+            element={<UserProfile />}
+          />
+          <Route path={"/company-profile"} element={<CompanyProfile />} />
+          <Route path={"/company-profile/:id"} element={<CompanyProfile />} />
+          <Route path={"/upload-job"} element={<UploadJob />} />
+          <Route path={"/job-detail/:id"} element={<JobDetail />} />
+        </Route>
+        <Route path='/about-us' element={<About />} />
+        <Route path='/user-auth' element={<Auth />} />
+      </Routes>
+
+      {user && <Footer />}
+    </main>
   );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,19 @@
+import About from "./About";
+import Auth from "./Auth";
+import Companies from "./Companies";
+import CompanyProfile from "./CompanyProfile";
+import FindJobs from "./FindJobs";
+import JobDetail from "./JobDetail";
+import UploadJob from "./UploadJob";
+import UserProfile from "./UserProfile";
+
+export {
+  About,
+  Auth,
+  Companies,
+  CompanyProfile,
+  FindJobs,
+  JobDetail,
+  UploadJob,
+  UserProfile,
+};
