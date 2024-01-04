1795486cc2085126f517883f6f9e722d6bfc052b
mdshahco <mdshahcoding.gmail.com>
2024-01-04 21:45:06 +0600

    starter template with react and tailwind css, and add assets

---
+++ client/src/assets/index.js
@@ -0,0 +1,41 @@
+import UserProfile from "./userprofile.png";
+import HeroImg from "./hero.png";
+import JobImg from "./job.jpg";
+import OfficeImg from "./office.jpg";
+import Microsoft from "./microsoft.png";
+import Google from "./google.png";
+import Meta from "./meta.png";
+import Linkedin from "./linkedin.png";
+import Youtube from "./youtube.png";
+import WhatsApp from "./whatsapp.png";
+import X from "./x.png";
+import Instagram from "./instagram.png";
+import Pinterest from "./pinterest.png";
+import Spotify from "./spotify.png";
+import Amazon from "./amazon.png";
+import Apple from "./apple.png";
+import Hp from "./hp.png";
+import Ibm from "./ibm.png";
+
+
+
+export {
+  UserProfile,
+  HeroImg,
+  JobImg,
+  OfficeImg,
+  Microsoft,
+  Google,
+  Meta,
+  Linkedin,
+  Youtube,
+  WhatsApp,
+  X,
+  Instagram,
+  Pinterest,
+  Spotify,
+  Amazon,
+  Apple,
+  Hp,
+  Ibm,
+};
